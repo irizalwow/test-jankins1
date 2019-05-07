eecb404d1b98248047abda6777c533435d228df1
JAIRO CORDERO MASERO <0015263@insags.com>
2019-05-07 17:35:37 +0200

    remove unpackaged

--- unpackaged/aura/PruebaAPP/PruebaAPP.app
+++
@@ -1,3 +0,0 @@
-<aura:application>
-            <c:PruebaAuraRetrieve1/>
-</aura:application>
--- unpackaged/aura/PruebasFormSub/PruebasFormSubController.js
+++
@@ -1,5 +0,0 @@
-({
-    doInit : function(component, event, helper) {
-            console.log('HEMOS EMPEZADO AL MENOS');
-    }
-})
