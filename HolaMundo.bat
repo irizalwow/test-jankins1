f9a6df9717046f6d45177839502c7eb2a84c652c
JAIRO CORDERO MASERO <0015263@insags.com>
2019-05-07 12:33:40 +0200

    prueba de conexion

--- HolaMundo.bat
+++ HolaMundo.bat
@@ -1,5 +1,9 @@
 @echo off 
 echo HOLA MUNDO 
-sfdx force:auth:web:login -a test1 -r https://test.salesforce.com
+sfdx force:auth:jwt:grant --setalias test1Salesforce --clien
+tid 3MVG9T46ZAw5GTfUYFyrMkxQSft.VcPV_L2gLv8P39ZWN6ifSNkoPSM7CCS8GRbALrcMKCqSDDh6
+O1P4OH2UF --jwtkeyfile C:\_Jairo\SALESFOrCE\DX\certificados\certificates\server.
+key --username jairo.cordero.masero@bluewolf.com --instanceurl https://jcm-bluew
+olf1-dev-ed.my.salesforce.com/
 pause>nul 
 exit
