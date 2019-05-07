f5bee5a72b4fca52bcfcbcb62f8389551a7ad59f
JAIRO CORDERO MASERO <0015263@insags.com>
2019-05-07 16:49:47 +0200

    wadwa

--- HolaMundo.bat
+++ HolaMundo.bat
@@ -2,7 +2,7 @@
 echo HOLA MUNDO 
 ::sfdx force:auth:jwt:grant --setalias test1Salesforce --clientid 3MVG9T46ZAw5GTfUYFyrMkxQSft.VcPV_L2gLv8P39ZWN6ifSNkoPSM7CCS8GRbALrcMKCqSDDh6O1P4OH2UF --jwtkeyfile C:\_Jairo\SALESFOrCE\DX\certificados\certificates\server.key --username jairo.cordero.masero@bluewolf.com --instanceurl https://jcm-bluewolf1-dev-ed.my.salesforce.com/
 sfdx force:mdapi:retrieve -r C:\001_GIT\RETRIEVE -k C:\001_GIT\RETRIEVE\package.xml -u test1Salesforce
-C:\001_GIT\RETRIEVE\unzip.exe C:\001_GIT\RETRIEVE\unpackaged.zip
+C:\001_GIT\RETRIEVE\unzip.exe C:\001_GIT\RETRIEVE\unpackaged.zip -u
 
 pause>nul 
 exit
