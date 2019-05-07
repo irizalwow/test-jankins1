93f879538c0f3e76a3559cf67a10d04e9e338325
JAIRO CORDERO MASERO <0015263@insags.com>
2019-05-07 17:09:35 +0200

    awdwa

--- HolaMundo.bat
+++ HolaMundo.bat
@@ -3,6 +3,7 @@ echo HOLA MUNDO
 ::sfdx force:auth:jwt:grant --setalias test1Salesforce --clientid 3MVG9T46ZAw5GTfUYFyrMkxQSft.VcPV_L2gLv8P39ZWN6ifSNkoPSM7CCS8GRbALrcMKCqSDDh6O1P4OH2UF --jwtkeyfile C:\_Jairo\SALESFOrCE\DX\certificados\certificates\server.key --username jairo.cordero.masero@bluewolf.com --instanceurl https://jcm-bluewolf1-dev-ed.my.salesforce.com/
 sfdx force:mdapi:retrieve -r C:\001_GIT\RETRIEVE -k C:\001_GIT\RETRIEVE\package.xml -u test1Salesforce
 unzip.exe -o C:\001_GIT\RETRIEVE\unpackaged.zip
+git checkout master
 git add unpackaged
 git commit -m "pruebaJenkinsFile1"
 git push
